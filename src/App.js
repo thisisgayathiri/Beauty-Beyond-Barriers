07f40c719b163c440d75eeb5dbcc89745aac6008
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2023-03-11 18:10:43 +0530

    Removed eslint exclude and pushed antd

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import { NavLink } from 'react-router-dom';
-import './App.css';
+import { NavLink } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-
-          Home Screen... This asks for user details.
-
-          </div>
-        
-          <div>
           <NavLink to="/services">
-
             Click to go to what you are looking for screen
-
           </NavLink>
-
         </div>
-
-
-
       </header>
     </div>
   );
