a1babb39c1f9e19600549c3d3a95b40c00e68b2c
Gayathiri <gayathri28896@gmail.com>
2023-03-14 16:38:56 +0530

    Added services page

--- src/pages/onboarding/UserPreferences.jsx
+++ src/pages/onboarding/UserPreferences.jsx
@@ -1,11 +1,23 @@
+import { Button } from "antd";
 import React from "react";
+import {Redirect, useNavigate} from 'react-router-dom';
+import { COLORS } from "../../constants/colors";
 import Preferences from "../common/Preferences";
 import SubTitleHeader from "../common/SubTitleHeader";
 
 import TitleHeader from "../common/TitleHeader";
 import TitlePreviousPage from "../common/TitlePreviousPage";
 
+
+
 const UserPreferences = () => {
+
+  const navigate = useNavigate();
+
+  const confirm = () => {
+    navigate("/services");
+  }
+
   return (
     <div
       style={{
@@ -17,6 +29,28 @@ const UserPreferences = () => {
       <SubTitleHeader subTitle="And we'll help you find best services near you" />
 
       <Preferences />
+
+
+
+      <Button
+        onClick= {confirm}
+        size="large"
+        htmlType="submit"
+     
+        style={{
+          marginTop: "50px",
+          backgroundColor: COLORS.PRIMARY,
+          color: COLORS.PRIMARY_LIGHT,
+          fontWeight: 500,
+          width: "150px",
+          borderColor: COLORS.PRIMARY_LIGHT,
+         
+        }}
+        className="login-form-button"
+      >
+        Continue
+      </Button>
+
     </div>
   );
 };
