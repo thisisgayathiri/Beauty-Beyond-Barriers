1ff9d71f76a9ccd3b577bd549b7b51e0776ff77f
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2023-03-15 06:58:23 +0530

    Added icons to the preferences screen

--- src/pages/common/Preferences.jsx
+++ src/pages/common/Preferences.jsx
@@ -1,43 +1,57 @@
 import React from "react";
+import _ from 'lodash'
+import './Preferences.css'
 
 import { Card, List } from "antd";
+import AccessibilityIcon from "../svgs/Accessibility";
+import LiftIcon from "../svgs/Lift";
+import NoiseFreeIcon from "../svgs/NoiseFree";
+import HelpingHandIcon from "../svgs/HelpingHand";
+
+const getComponentbyIcon = (preference) => {
+  if(_.isEqual(preference, 'WHEELCHAIR'))
+    return <AccessibilityIcon />
+  else if (_.isEqual(preference, 'LIFT')) 
+    return <LiftIcon />
+  else if (_.isEqual(preference, 'NOISEFREE'))
+    return <NoiseFreeIcon />
+  else if (_.isEqual(preference, 'HELP'))
+    return <HelpingHandIcon />
+}
 
 const data = [
   {
     title: "Wheelchair accessible",
-    icon: "♿︎",
+    icon: "WHEELCHAIR",
   },
   {
     title: "Lift",
-    icon: "🛗",
-  },
-  {
-    title: "Ramps",
-    icon: "",
+    icon: "LIFT",
   },
   {
     title: "Noiseless",
-    icon: "🔇",
+    icon: "NOISEFREE",
   },
   {
-    title: "Silent rooms",
-    icon: "🤫",
+    title: "Helping hand",
+    icon: "HELP",
   },
 ];
 
 const Preferences = ({}) => {
   return (
     <List
+    
       grid={{
         gutter: 3,
       }}
       dataSource={data}
       renderItem={(item) => (
-        <List.Item>
+        <List.Item justify="space-around">
           <Card style={{
             padding: '0px 10px',
           }}>
-            <div>{item.icon}</div>
+            <span className='list-item-icon'>{getComponentbyIcon(item.icon)}</span>
             <span>{item.title}</span>
           </Card>
         </List.Item>
