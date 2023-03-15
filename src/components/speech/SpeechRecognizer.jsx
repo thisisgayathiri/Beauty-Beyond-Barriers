81efdf4f8e6062be7524477b8c7185b78527f34f
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2023-03-15 07:20:37 +0530

    Formatted the speech rec component and updated img

--- src/components/speech/SpeechRecognizer.jsx
+++ src/components/speech/SpeechRecognizer.jsx
@@ -1,78 +1,79 @@
-import React from 'react';
-import Modal from 'react-modal';
-import { useNavigate } from 'react-router';
-import SpeechRecognition from 'react-speech-recognition';
-import { useSpeechRecognition } from 'react-speech-recognition';
-
+import React from "react";
+import Modal from "react-modal";
+import { useNavigate } from "react-router";
+import SpeechRecognition from "react-speech-recognition";
+import { useSpeechRecognition } from "react-speech-recognition";
 
 const SpeechRecognizer = () => {
+  const navigate = useNavigate();
+  Modal.setAppElement("#root");
 
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      width: "150px",
+      height: "100px",
+      transform: "translate(-50%, -50%)",
+    },
+  };
 
-    const navigate = useNavigate();
-    Modal.setAppElement('#root');
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
-    const customStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            width: '150px',
-            height: '100px',
-            transform: 'translate(-50%, -50%)',
-        },
-    };
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
+  if (transcript.toLowerCase().includes("search")) {
+    setTimeout(() => {
+      navigate("/salons");
+    }, 1000);
+  }
 
-    const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition
-    } = useSpeechRecognition();
+  function openModal() {
+    setIsOpen(true);
+  }
 
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+    SpeechRecognition.startListening();
+  }
 
+  function closeModal() {
+    setIsOpen(false);
+    SpeechRecognition.stopListening();
+  }
 
-    const [modalIsOpen, setIsOpen] = React.useState(false);
+  return (
+    <div>
+      <img
+        onClick={openModal}
+        style={{ position: "fixed", bottom: "1%", right: "5%" }}
+        src="/images/mic.svg"
+      />
 
-    if(transcript.toLowerCase().includes('search')) {
-        setTimeout(() => {
-            navigate("/salons")
-        }, 1000);
-    }
-
-    function openModal() {
-        setIsOpen(true);
-    }
-
-    function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        // subtitle.style.color = '#f00';
-        SpeechRecognition.startListening();
-    }
-
-    function closeModal() {
-        setIsOpen(false);
-        SpeechRecognition.stopListening();
-    }
-
-    return (
-        <div>
-            <img onClick={openModal} style={{ "position": "fixed", "bottom": "2%", "right": "5%", "width": "50px" }} src="/images/mic.jpeg"></img>
-
-            <Modal
-                isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Speech Modal"
-            >
-                <div style={{color: "black", display: transcript==''? '': 'none'}}>Listening..</div>
-                <div style={{ color: "black" }}>{transcript}</div>
-
-            </Modal>
-        </div>);
-    ;
-}
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Speech Modal"
+      >
+        <div
+          style={{ color: "black", display: transcript == "" ? "" : "none" }}
+        >
+          Listening..
+        </div>
+        <div style={{ color: "black" }}>{transcript}</div>
+      </Modal>
+    </div>
+  );
+};
 export default SpeechRecognizer;
