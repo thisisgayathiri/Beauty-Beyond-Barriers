dcaaff6077b15a71ba0fee5f2891d6253f4fd6a7
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2023-03-16 07:35:27 +0530

    Implemented the menu tab component

---
+++ src/components/common/TabMenu.jsx
@@ -0,0 +1,49 @@
+import { Affix, Tabs } from "antd";
+import React from "react";
+import menuItems from "../../data/menu";
+import {
+  AppstoreTwoTone,
+  HeartTwoTone,
+  ScheduleTwoTone,
+} from "@ant-design/icons";
+import { COLORS } from "../../constants/colors";
+
+const TabMenu = () => {
+  const getIcon = (icon) => {
+    if (_.isEqual(icon, "explore"))
+      return <AppstoreTwoTone twoToneColor={COLORS.PRIMARY} />;
+    else if (_.isEqual(icon, "bookings"))
+      return <ScheduleTwoTone twoToneColor={COLORS.PRIMARY} />;
+    else if (_.isEqual(icon, "account"))
+      return <HeartTwoTone twoToneColor={COLORS.PRIMARY} />;
+  };
+
+  return (
+    <Affix offsetBottom={0}>
+      <Tabs
+        style={{
+          backgroundColor: "white",
+          padding: "0 20px",
+        }}
+        defaultActiveKey="2"
+        items={menuItems.map(({ name, icon, id }) => {
+          return {
+            label: (
+              <span
+                style={{
+                  backgroundColor: "white",
+                }}
+              >
+                {getIcon(icon)}
+                {name}
+              </span>
+            ),
+            key: id,
+          };
+        })}
+      />
+    </Affix>
+  );
+};
+
+export default TabMenu;
