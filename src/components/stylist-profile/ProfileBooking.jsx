1514d578e915abe9fdbdc14730c1ffd76860b83c
Sowmya Seshadri <seshadrisowmya5@gmail.com>
2023-03-19 19:25:47 +0530

    profile

--- src/components/stylist-profile/ProfileBooking.jsx
+++ src/components/stylist-profile/ProfileBooking.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from 'react';
+import MyDatePicker from "../../components/common/MyDatePicker";
+import Card from "antd/es/card/Card";
+import { Radio, Space } from 'antd';
+import Title from "antd/es/typography/Title";
+import { COLORS } from "../../constants/colors";
+
+const options = [
+  { label: '[09:00 AM - 12:00 AM]', value: 'Morning',disabled: true },
+  { label: '[01:00 PM -04:00 PM]', value: 'Afternoon'},
+  { label: '[05:00 PM - 08:00 PM]', value: 'Evening' },
+  { label: '[08:00 PM - 10:00 PM]', value: 'Night' },
+];
 
 const ProfileBooking = () => {
-  return <></>;
+
+  
+  const [value, setValue] = useState('Choose Your Favorable Time Slot !');
+  
+  const onChange3 = ({ target: { value } }) => {
+    console.log('radio3 checked', value);
+    setValue(value);
+  };
+  
+  return <>
+    <Space direction='vertical'>
+  <MyDatePicker />
+   <Card>
+   <Space direction='vertical'>
+   <Title
+        style={{
+          padding: "0 0px 0px 0px",
+          fontWeight: 400,
+          color: COLORS.SECONDARY,
+        }}
+        level={5}
+      >
+        Book Your Favorable Slot
+      </Title>
+      <br/><br/>
+  <Radio.Group options={options} onChange={onChange3} value={value} buttonStyle="solid" optionType="button" 
+  style={{ width: "250px"}}/>
+   </Space>
+  </Card>
+ </Space>
+  </>;
 };
 
 export default ProfileBooking;
--- src/components/stylist-profile/ProfileService.jsx
+++ src/components/stylist-profile/ProfileService.jsx
@@ -1,7 +1,90 @@
 import React from "react";
+import { Divider, Table,Button, Space } from 'antd';
+
+import { COLORS } from "../../constants/colors";
+  
+const columns = [
+  {
+    dataIndex: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    dataIndex: 'age',
+  },
+  {
+    dataIndex: 'address',
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'Thumbnail',
+    age: 'Hair Stylist',
+    address: 'Ace',
+  },
+  {
+    key: '2',
+    name: 'Thumbnail',
+    age: 'Nail Treatments',
+    address: 'Platinum',
+  },
+  {
+    key: '3',
+    name: 'Thumbnail',
+    age: 'Facial and Skin Care',
+    address: 'Gold',
+  },
+  {
+    key: '4',
+    name: 'Thumbnail',
+    age: 'Massages',
+    address: 'Gold',
+  },
+
+];
+
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === 'Disabled User',
+    // Column configuration not to be checked
+    name: record.name,
+  }),
+};
 
 const ProfileService = () => {
-  return <></>;
+  return (
+    <div>
+      <Table pagination={false} showHeader={false}
+        rowSelection={{
+          type: "checkbox",
+          ...rowSelection,
+        }}
+        columns={columns}
+        dataSource={data}
+      />
+      <div>
+      <Space direction="vertical" style={{ width: '100%' }}>
+    
+    <Button disabled block
+            size="large"
+            htmlType="submit"
+            style={{
+              backgroundColor: COLORS.PRIMARY,
+              color: COLORS.PRIMARY_LIGHT,
+              fontWeight: 500,
+              borderColor: COLORS.PRIMARY_LIGHT,
+            }}className="login-form-button"
+            >
+              Book Slots
+            </Button>
+        </Space>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileService;
